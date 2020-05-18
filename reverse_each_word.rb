42fb4e1e294518ff2a2ad7c6dd16d0899508e278
Samuel Brigha <sam.brigham@yale.edu>
2020-05-18 15:52:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def reverse_each_word(string)
 array1 = string.split(" ")  
-new_array = array1.collect do |element| 
+array1.collect do |element| 
   element.reverse
   binding.pry
 end
