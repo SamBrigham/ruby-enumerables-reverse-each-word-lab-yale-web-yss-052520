00ffb12d02484050f4f53db8844cc2339bc2cd4b
Samuel Brigha <sam.brigham@yale.edu>
2020-05-18 15:42:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def reverse_each_word(string)
 array1 = string.split(" ")  
-binding.pry
 new_array = array1.collect do |element| 
   element.reverse
+  binding.pry
 end
 puts new_array
 end
