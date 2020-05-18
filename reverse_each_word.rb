b451a5cf3c9aaaca242ec552760b0893c0fdf1d3
Samuel Brigha <sam.brigham@yale.edu>
2020-05-18 15:59:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
 array1 = string.split(" ")
 new_array = []
 new_array = array1.collect do |element| 
-  element.reverse
+  new_array << element.reverse
   binding.pry
 end
 puts new_array
