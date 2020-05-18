f3d685f39ab6b264d78d3ba03d2bb84498dc0bbe
Samuel Brigha <sam.brigham@yale.edu>
2020-05-18 16:02:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reverse_each_word(string)
 array1 = string.split(" ")
 new_array = []
-new_array = array1.collect do |element| 
+array1.collect do |element| 
   new_array << element.reverse
 end
 puts new_array
